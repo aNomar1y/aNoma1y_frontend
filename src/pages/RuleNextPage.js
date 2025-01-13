d2e1578b353949ad8d3851d3395e37dc4221e9b9
sanghyuk-ko <90sangko@naver.com>
2025-01-13 22:15:06 +0900

    [fix] cctv end

--- src/pages/RuleNextPage.js
+++ src/pages/RuleNextPage.js
@@ -9,7 +9,7 @@ function RuleNextPage() {
     <div className="rule-container">
       <h1>&lt;N1 관리자 행동수칙&gt;</h1>
       <ol>
-        <li>귀하가 현재 계신 곳은 본관의 1층 114호입니다.</li>
+        <li>귀하가 현재 계신 곳은 본관의 1층 CCTV실입니다.</li>
         <li>화면에 나타나는 모든 것은 의미가 있습니다.</li>
         <li>변화에 대한 보고는 즉각적으로 하십시오.</li>
         <li>불필요한 보고는 관제센터의 신뢰를 잃게 만듭니다.</li>
