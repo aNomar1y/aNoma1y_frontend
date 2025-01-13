060c5105c91264c7fca1d1b31c3d8127a50efe42
hyunwoo <hahachw@kaist.ac.kr>
2025-01-13 16:15:52 +0900

    01.13.16.15

--- src/BgmController.js
+++ src/BgmController.js
@@ -4,8 +4,10 @@ import { useLocation } from "react-router-dom";
 const BgmController = ({ shouldPlay }) => {
   const location = useLocation();
   const audioRef = useRef(null); // 오디오 요소 참조
+  const typingAudioRef = useRef(null); // 추가 사운드 키보드 타이핑
   const bgmLoginMain = "/assets/sounds/lobby.m4a"; // 로그인 및 메인 BGM
   const bgmOther = "/assets/sounds/noise.m4a"; // 나머지 화면 BGM
+  const bgmtyping = "/assets/sounds/keyboard-typing.mp3";
 
   useEffect(() => {
     const currentPath = location.pathname;
@@ -32,9 +34,26 @@ const BgmController = ({ shouldPlay }) => {
         audioRef.current.pause();
       }
     }
+
+    if (typingAudioRef.current) {
+      if(currentPath === "/rule") {
+        typingAudioRef.current.src = bgmtyping;
+        typingAudioRef.current.play().catch((err) => {
+          console.error("Typing Sound play error", err);
+        });
+      } else {
+        typingAudioRef.current.pause();
+        typingAudioRef.current.currentTime = 0;
+      }
+    }
   }, [location.pathname, shouldPlay]); // 경로 또는 shouldPlay 값이 변경될 때 실행
 
-  return <audio ref={audioRef} loop />;
+  return (
+    <>
+    <audio ref={audioRef} loop />
+    <audio ref={typingAudioRef} />
+    </>
+  );
 };
 
 export default BgmController;
