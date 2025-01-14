7b70c286f52406f9cdbddb09756f26e36acd6b90
hyunwoo <hahachw@kaist.ac.kr>
2025-01-14 16:20:20 +0900

    01.14.16.20

--- src/pages/WinPage.js
+++ src/pages/WinPage.js
@@ -6,6 +6,7 @@ const WinPage = () => {
   const [showVideo, setShowVideo] = useState(false); // 동영상 표시 여부
   const navigate = useNavigate();
   const audioRef = useRef(null);
+  const glitchAudioRef = useRef(null);
 
   useEffect(() => {
     // 2초 후 동영상 재생
@@ -26,21 +27,37 @@ const WinPage = () => {
 
   // showVideo 상태에 따라 사운드 재생을 제어
   useEffect(() => {
+    // 클리어 사운드 재생
     if(!showVideo) {
       if (audioRef.current) {
         audioRef.current.loop = true;
-        audioRef.current.volume = 0.5;
+        audioRef.current.volume = 1;
         audioRef.current.play().catch((err) => {
           console.error("audio play error:", err);
         });
       }
+
+      //글리치 사운드 중지
+      if (glitchAudioRef.current) {
+        glitchAudioRef.current.pause();
+        glitchAudioRef.current.currentTime = 0;
+      }
     } else {
+      // 글리치 사운드 재생
+      if (glitchAudioRef.current) {
+        glitchAudioRef.current.loop = true;
+        glitchAudioRef.current.volume = 0.3;
+        glitchAudioRef.current.play().catch((err) => {
+          console.error("glitch sound play error:", err);
+        })
+      }
       if (audioRef.current) {
         audioRef.current.pause();
         audioRef.current.currentTime = 0;
       }
     }
   }, [showVideo]);
+  
 
   return (
     <div className="win-container">
@@ -48,6 +65,12 @@ const WinPage = () => {
       <audio
         ref={audioRef}
         src="/assets/sounds/win-bell.mp3"
+        preload="auto"
+      />
+      {/* 클리어 사운드 */}
+      <audio
+        ref={glitchAudioRef}
+        src="/assets/sounds/glitch.mp3"
         preload="auto"
       />
       {/* 클리어 텍스트 */}
